00eb8a4b07d44ac62cb7c4d49d0f18cbe8f091bc
Chidubem Emmanuel <obuchidubem@gmail.com>
2026-08-08 19:54:45 +0100

    done profile-card

--- profile-card/src/App.jsx
+++ profile-card/src/App.jsx
@@ -1,13 +1,42 @@
+import profile from "./assets/profile.jpg";
 
 function App() {
-
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-5xl font-bold text-blue-500">
-        Tailwind is working 🚀
-      </h1>
+      <div className="border-2 border-pink-950 rounded-lg p-6">
+        <img
+          className="w-48 h-48 rounded-full border-2 border-pink-950 mx-auto"
+          src={profile}
+        />
+
+        <div className="my-5">
+          <div className="flex gap-2">
+            <p className="font-bold">Name:</p>
+            <p>John</p>
+          </div>
+
+          <div className="flex gap-2">
+            <p className="font-bold">Job:</p>
+            <p>Software Engineer</p>
+          </div>
+
+          <div className="flex gap-2">
+            <p className="font-bold">Location:</p>
+            <p>Nigeria</p>
+          </div>
+
+          <div className="flex gap-2">
+            <p className="font-bold">Bio:</p>
+            <p>Software Engineering Student</p>
+          </div>
+        </div>
+
+        <button className="border-2 border-pink-950 px-4 py-2 rounded-lg mx-auto block">
+          View Profile
+        </button>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
